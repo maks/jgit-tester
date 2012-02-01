e57eaa250d689506823428edfc2c6b5e561fd90a
Maksim Lin <maks@manichord.com>
2012-02-01 11:16:40 +1100

    wip on testing autheticated git+ssh repo cloning.

--- src/GitTester.java
+++ src/GitTester.java
@@ -1,19 +1,19 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PullCommand;
-import org.eclipse.jgit.api.errors.CanceledException;
-import org.eclipse.jgit.api.errors.DetachedHeadException;
-import org.eclipse.jgit.api.errors.InvalidConfigurationException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.NoHeadException;
-import org.eclipse.jgit.api.errors.RefNotFoundException;
-import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.JschConfigSessionFactory;
+
+import com.jcraft.jsch.UserInfo;
 
 
 public class GitTester {
@@ -23,22 +23,50 @@ public class GitTester {
 		String URL = "https://maks@github.com/maks/sandbox.git";
 		
 		Repository repository = new FileRepository("/tmp/testrepo/.git");
+		if (args[0].equalsIgnoreCase("ac")) {
+			authenticatedCloneRepo(new File("/home/maks/.ssh/maksgithub_id_rsa"));
+		}
 		if (args[0].equalsIgnoreCase("c")) {
-			cloneRepo(PATH, URL);
+			cloneRepo(PATH, URL, null);
 		}
 		if (args[0].equalsIgnoreCase("p")) {
 			pullRepo(repository);
+		}
+		if (args[0].equalsIgnoreCase("h")) {
+			getHead(repository);
 		} else {
 			System.err.println("Invalid option:"+args[0]);
 		}
 	}
 	
-	private static void cloneRepo(File path, String url) {
-		              
+	private static void authenticatedCloneRepo(File privKeyFile) {
+		String prvKey = null;
+		try {
+			prvKey = streamToString(new FileInputStream(privKeyFile));
+			UserInfo userInfo = null;
+			GitTesterConfigSessionFactorty sshFactory = new GitTesterConfigSessionFactorty(
+					userInfo, prvKey);
+			JschConfigSessionFactory.setInstance(sshFactory);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void getHead(Repository repository) throws IOException {
+		System.out.println(repository.getRef("master").getObjectId().getName());
+	}
+
+	private static void cloneRepo(File path, String url,
+			CredentialsProvider credentialsProvider) {
+
 		CloneCommand clone = Git.cloneRepository();
 		clone.setBare(false);
-		clone.setCloneAllBranches(false);		
-		clone.setDirectory(path).setURI(url);		
+		clone.setCloneAllBranches(false);
+		clone.setDirectory(path).setURI(url);
+
+		if (credentialsProvider != null) {
+			clone.setCredentialsProvider(credentialsProvider);
+		}
 		clone.call();   
 	}
 	
@@ -52,4 +80,16 @@ public class GitTester {
 			e.printStackTrace();
 		}
 	}
+
+	public static String streamToString(InputStream inStream)
+			throws IOException {
+		BufferedReader r = new BufferedReader(new InputStreamReader(inStream));
+		StringBuilder total = new StringBuilder();
+		String line;
+		while ((line = r.readLine()) != null) {
+			total.append(line);
+		}
+		return total.toString();
+	}
+
 }
---
+++ src/GitTesterConfigSessionFactorty.java
@@ -0,0 +1,37 @@
+import org.eclipse.jgit.transport.JschConfigSessionFactory;
+import org.eclipse.jgit.transport.OpenSshConfig.Host;
+import org.eclipse.jgit.util.FS;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import com.jcraft.jsch.UserInfo;
+
+
+public class GitTesterConfigSessionFactorty extends JschConfigSessionFactory {
+
+	private final UserInfo userInfo;
+	private String prvkey;
+
+	public GitTesterConfigSessionFactorty(UserInfo userInfo, String prvKey) {
+		this.userInfo = userInfo;
+		this.prvkey = prvKey;
+	}
+
+	@Override
+	protected void configure(Host host, Session session) {
+		session.setConfig("StrictHostKeyChecking", "no"); // DON'T let the
+															// hostKeyRepository
+															// ask the questions
+		session.setUserInfo(userInfo);
+
+	}
+
+	@Override
+	protected JSch createDefaultJSch(FS fs) throws JSchException {
+		final JSch jsch = new JSch();
+		jsch.addIdentity(prvkey);
+		return jsch;
+	}
+
+}
